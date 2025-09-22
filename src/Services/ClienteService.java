5995a379e44f281b002baf1038c500fb60a99ff2
Gustavo Marques Reis <gustavomreis.dev@gmail.com>
2025-09-22 19:50:05 -0300

    Update ClienteService.java
    Atualização do service para usar a classe DAO.

--- src/Services/ClienteService.java
+++ src/Services/ClienteService.java
@@ -1,54 +1,48 @@
 package Services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import Entities.Cliente;
 import Entities.Endereco;
+import dao.ClienteDAO;
 
 public class ClienteService {
-
-	private List<Cliente> lista;
-
-	public ClienteService() {
-		lista = new ArrayList<Cliente>();
-	}
 	
 	public void adicionarCliente(Cliente cliente) {
-		boolean existe = lista.stream().anyMatch(c -> c.getCpf().equals(cliente.getCpf()));
+		boolean existe = ClienteDAO.buscarClienteByCpf(cliente.getCpf()).isPresent();
 	
 		if(existe) {
 			throw new IllegalArgumentException("Cliente já cadastrado com esse cpf.");
 		}
-		lista.add(cliente);
+		ClienteDAO.incluirCliente(cliente);
 	}
 	
 	public Cliente buscarClientePorCpf(String cpf) {
-		return lista.stream()
-				.filter(c -> c.getCpf().equals(cpf))
-				.findFirst()
-				.orElse(null);
+		Optional<Cliente> clienteOptional = ClienteDAO.buscarClienteByCpf(cpf);
+		if (clienteOptional.isEmpty()) {
+			return null;
+		}
+		return clienteOptional.get();
 	}
 	
 	public Cliente buscarClientePorNome(String nome) {
-		return (Cliente) lista.stream()
-				.filter(c -> c.getNome().equalsIgnoreCase(nome))
-				.collect(Collectors.toList());
+		Optional<Cliente> clienteOptional = ClienteDAO.buscarClienteByNome(nome);
+		if (clienteOptional.isEmpty()) {
+			return null;
+		}
+		return clienteOptional.get();
 	}
 	
 	public List<Cliente> listarClientes() {
-		return lista;
+		return ClienteDAO.listarTodosClientes();
 	}
 	
 	public void editarCliente(String cpf, String novoNome, String novoEmail, Endereco endereco) {
-		Cliente cliente = buscarClientePorCpf(cpf);
-		if(cliente == null) {
+		Optional<Cliente> clienteOptional = ClienteDAO.buscarClienteByCpf(cpf);
+		if (clienteOptional.isEmpty()) {
 			throw new IllegalArgumentException("Cliente não localizado.");
 		}
-		cliente.setNome(novoNome);
-		cliente.setEmail(novoEmail);
-		cliente.setEndereco(endereco);
-		
+		ClienteDAO.editarCliente(cpf, novoNome, novoEmail, endereco);
 	}
 }
